50344f05a1480d5c8bd6c99e625a6e939b220669
charliram93-png <charliram93@gmail.com>
2026-03-05 20:34:48 -0600

    fix icon mobile 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 
   openGraph: {
     title: "Indego Studio",
-    description: "Under Construction - Drop #1",
+    description: "Under Construction",
     url: "https://indegostudio.com",
     siteName: "Indego Studio",
     images: [
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Indego Studio",
-    description: "Under Construction - Drop #1",
+    description: "Under Construction",
     images: ["https://res.cloudinary.com/dij60ghdf/image/upload/v1772763867/LogoWhatsMetaData_jmp0lg.png"], 
   },
 };
