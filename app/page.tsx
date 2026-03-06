12e5b18bceb2c0456368811313e834b7a5c90651
charliram93-png <charliram93@gmail.com>
2026-03-05 18:37:46 -0600

    3 UNDERCONSTRUCTION

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,59 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white"
       style={{
-        backgroundColor: "#888C50",
-        fontFamily: "Helvetica, Arial, sans-serif"
+        backgroundColor: "#32331F",
+        fontFamily: "Inter, sans-serif",
+        fontWeight: "600",
       }}
+      
     >
-      <div className="text-center space-y-10">
+      <div className="header">
+        <Image src="https://res.cloudinary.com/dij60ghdf/image/upload/v1772753917/Logo_White_xhx1kd.webp" width={100} height={100} alt="Logo" className="logo" 
+        style={{
+          position: "fixed",   /* se queda arriba */
+          top: 0,
+          left: 0,
+          
+        }}/>
+      </div>
 
-        <h1 className="text-4xl md:text-6xl tracking-[0.25em] font-light">
-          INDEGO STUDIO
-        </h1>
+      <body className="text-center space-y-10">
 
-        <p className="text-sm tracking-[0.5em] opacity-80">
-          UNDER CONSTRUCTION
-        </p>
-
-        <div className="pt-12">
-          <p className="text-lg tracking-[0.4em]">
-            DROP 1
-          </p>
-
-          <p className="text-sm tracking-[0.5em] opacity-80 mt-2">
-            COMING SOON
-          </p>
+        <div className="text-4xl  tracking-[0.25em] ">
+          <Image src="https://res.cloudinary.com/dij60ghdf/image/upload/f_auto,q_auto/v1772755253/LogoHev_White_db30dd.png" width={600} height={600} alt="shirt" />
         </div>
 
-      </div>
+        <div className="text-4xl  tracking-[0.25em] ">
+          <Image src="https://res.cloudinary.com/dij60ghdf/image/upload/f_auto,q_auto/v1772755266/LogoKids_White_ril01y.png" width={600} height={600} alt="shirt" />
+        </div>
+
+        <div className="pt-12">
+          <h1 className="text-4xl">
+            UNDER CONSTRUCTION
+          </h1>
+          <div>
+            <p className="text-lg mt-2">
+            DROP #1
+            </p>
+            <p className="text-lg mt-2">
+            COMING SOON
+            </p>
+          </div>
+        </div>
+
+        <div className="text-4xl  tracking-[0.25em] ">
+          <Image src="https://res.cloudinary.com/dij60ghdf/image/upload/f_auto,q_auto/v1772755261/Indg_Cd_White_zqimyq.png" width={115} height={5
+          } alt="shirt" />
+        </div>
+
+      </body>
+
+      <header>
+        
+      </header>
     </main>
   );
 }
